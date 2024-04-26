7ada5e0ae235e9e9ad8a8d98090652df7a4fc6ee
huameiwei-vc <125441847+huameiwei-vc@users.noreply.github.com>
2024-04-26 17:27:00 +0800

    Update cui.py

--- kaggle/cui/asd/cui.py
+++ kaggle/cui/asd/cui.py
@@ -2,10 +2,9 @@ import subprocess
 import os
 
 minyak = [
-    ['rm', '-rf', '/kaggle/temp/*', '/kaggle/temp', '/kaggle/working/ComfyUI/models/checkpoints', '/kaggle/working/ComfyUI/models/loras', '/kaggle/working/ComfyUI/models/controlnet'],
+    ['rm', '-rf', '/kaggle/temp/*', '/kaggle/temp', '/kaggle/working/ComfyUI/models/checkpoints', '/kaggle/working/ComfyUI/models/loras'],
     ['ln', '-vs', '/kaggle/temp/checkpoints', '/kaggle/working/ComfyUI/models/checkpoints'],
     ['ln', '-vs', '/kaggle/temp/loras', '/kaggle/working/ComfyUI/models/loras'],
-    ['ln', '-vs', '/kaggle/temp/controlnet', '/kaggle/working/ComfyUI/models/controlnet'],
     ['unzip', '-o', '/kaggle/working/ComfyUI/models/embeddings.zip', '-d', '/kaggle/working/ComfyUI/models/embeddings'],
     ['rm', '/kaggle/working/ComfyUI/models/embeddings.zip']
 ]
